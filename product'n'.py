4e660bdbfc78645047235e4ff1a5ca3b46ce764b
babapubg <72243070+babapubg@users.noreply.github.com>
2020-10-17 09:17:50 +0530

    Add files via upload

---
+++ product'n'.py
@@ -0,0 +1,8 @@
+pro=1
+n=int(input("Enter the total numbers to be multiplied : "))
+i=1
+while i <= n:
+    a = int(input("Enter the number at "+str(i)+" position : "))
+
+    i = i+1
+print("Product of " + str(n) +" numbers = ",pro)
